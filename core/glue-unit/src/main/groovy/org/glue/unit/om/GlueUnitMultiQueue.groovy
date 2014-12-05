6a96ce138b32be794ae767d5a446ce9afa55a99b
MillerChristopherE <cmiller@myspace-inc.com>
2014-12-05 15:24:02 -0800

    Support for multiple queues

---
+++ core/glue-unit/src/main/groovy/org/glue/unit/om/GlueUnitMultiQueue.groovy
@@ -0,0 +1,16 @@
+package org.glue.unit.om;
+
+
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ */
+@Typed
+public interface GlueUnitMultiQueue extends GlueUnit {
+
+    String getQueue();
+    void setQueue(String name);
+
+}
